7debb85441f390a31b260e6726896017b3fa50eb
ArimotoYui <akasatana05280708@gmail.com>
2023-09-10 09:41:31 +0900

    ログイン

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <h1>Blog Name</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('index') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                            <h1>Blog Name</h1>
         <a href='/posts/create'>create</a>
         <div class='posts'>
             @foreach ($posts as $post)
@@ -36,5 +38,8 @@
                 }
             }
         </script>
-    </body>
-</html>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
